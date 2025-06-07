508672726b1c49c91f64f3599d3926fd6f2dcef9
Sven Steinbauer <sven@unlogic.co.uk>
2025-06-07 07:28:27 +0100

    Update main code for new kmk library

--- code.py
+++ code.py
@@ -1,15 +1,16 @@
 print("Starting")
 
 import board
+import busio
 
 from kmk.kmk_keyboard import KMKKeyboard
-from kmk.consts import UnicodeMode
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
 from kmk.extensions.media_keys import MediaKeys
-from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
+from kmk.extensions.display import Display, TextEntry
+from kmk.extensions.display.ssd1306 import SSD1306
 from kmk.extensions.rgb import AnimationModes
 from kmk.extensions.RGB import RGB
 
@@ -33,19 +34,30 @@ encoder_handler.pins = (
 )
 
 # OLED display
-keyboard.SCL=board.D5
-keyboard.SDA=board.D4
+i2c_bus = busio.I2C(board.D5, board.D4)
 
-oled_ext = Oled(
-    OledData(
-        corner_one={0:OledReactionType.STATIC,1:["layer"]},
-        corner_two={0:OledReactionType.LAYER,1:["Numpad", "Fn"]},
-        corner_three={0:OledReactionType.STATIC,1:[""]},
-        corner_four={0:OledReactionType.STATIC,1:[""]},
-        ),
-        toDisplay=OledDisplayMode.TXT,flip=False)
+driver = SSD1306(i2c=i2c_bus)
+display = Display(
+    display=driver,
+    dim_time=20, # time in seconds to reduce screen brightness
+    dim_target=0.1, # set level for brightness decrease
+    off_time=60, # time in seconds to turn off screen
+    powersave_dim_time=10, # time in seconds to reduce screen brightness
+    powersave_dim_target=0.1, # set level for brightness decrease
+    powersave_off_time=30, # time in seconds to turn off screen
+)
+display.entries = [
+    TextEntry(text='Layer: ', x=0, y=32, y_anchor='B'),
+    TextEntry(text='BASE', x=40, y=32, y_anchor='B', layer=0),
+    TextEntry(text='NUM', x=40, y=32, y_anchor='B', layer=1),
+    TextEntry(text='NAV', x=40, y=32, y_anchor='B', layer=2),
+    TextEntry(text='0 1 2', x=0, y=4),
+    TextEntry(text='0', x=0, y=4, inverted=True, layer=0),
+    TextEntry(text='1', x=12, y=4, inverted=True, layer=1),
+    TextEntry(text='2', x=24, y=4, inverted=True, layer=2),   
+]
 
-keyboard.extensions.append(oled_ext)
+keyboard.extensions.append(display)
 
 # LEDs
 rgb = RGB(pixel_pin=board.NEOPIXEL, num_pixels=1, val_limit=255, val_step=2, val_default=255,
@@ -89,4 +101,4 @@ encoder_handler.map = [
 ]
 if __name__ == '__main__':
     keyboard.go()
-    
+ 
